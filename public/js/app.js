afb6083c6b4ade210acfd7ea95649bc124fef4d8
zotherstupidguy <zotherstupidguy@gmail.com>
2016-08-14 16:26:03 +0200

    adds a working cookie based access token via jquery ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,64 @@
 $(document).ready(function(){
-  $("button").click(function(){
-    $("#div1").load("/play",function(responseTxt,statusTxt,xhr){
-      /* if(statusTxt=="success") alert("External content loaded successfully!"); */
-      if(statusTxt=="error")
-        alert("Error: "+xhr.status+": "+xhr.statusText);
-    });
+  $("#login").click(function(){
+    $("input").prop('required',true);
+
+    if ($("#login_form").valid()) {
+      var login_data = $("#login_form").serialize();
+
+
+      $.ajax({
+	type: "POST",
+	url: "/auth",
+	data: login_data,
+	success: function(data){ 
+	  console.log(data); 
+	  Cookies.set('access_token', data["access_token"]);
+	},
+	dataType: "json"
+	//dataType: dataType
+      });
+      return false;
+
+
+      $.ajax({
+	url: "/channels",
+	data: Cookies.get('access_token'),
+	success: function(data){ 
+	  // get channel url
+	  console.log(Cookies.get('access_token'));
+	  console.log("use it to get the channel"); 
+	},
+	dataType: "json"
+      });
+
+      /*
+	 $.post("/login", $("#login_form").serialize() , function(data,status){ 
+	 alert("Data: " + data + "\nStatus: " + status); 
+	 console.log(login_data);
+	 $.get( "/channels", function( data ) {
+      //$( "#player_area" ).html( data );
+      Cookies.set('name', 'value');
+      Cookies.set('name', 'value', { expires: 365 });
+      Cookies.get('name'); // => 'value'
+      Cookies.remove('name');
+      alert( "Load was performed." );
+      });
+      //TODO add if statment here if login status success then load using access token
+      //$("#player_area").load("/channels",function(responseTxt,statusTxt,xhr){
+      //$("#player_area").load("/channels",function(responseTxt,statusTxt,xhr){
+      //  console.log("hi");
+      // if(statusTxt=="success") alert("External content loaded successfully!"); 
+      // if(statusTxt=="error")
+      //  alert("Error: "+xhr.status+": "+xhr.statusText);
+      //});
+
+      }); 
+      */ 
+
+      }
+
+
+
+      });
   });
-});
 
--- sinatra_jquery_test.rb
+++ sinatra_jquery_test.rb
@@ -1,15 +1,32 @@
 require 'sinatra'
+require "sinatra/json"
 
 get '/' do
   erb :app
 end
 
-get '/play' do 
+get '/channels' do 
+  p "channels endpoint hit"
+  if params[:session_id] then
+    p json :url => 'thebigurl'
+    return json :url => 'thebigurl'
+  else
+    p "not authorized"
+    return "not authorized"
+  end
+=begin
   if request.xhr?
     #%q{<h1 class="blue">Hello! <a href="/">back</a></h1>}
     '<h1 class="blue">Hello! <a href="/">back</a></h1>'
+    json :url => 'thebigurl'
   else
     "<h1>Not an Ajax request!</h1>"
   end
+=end
 end
 
+post '/auth' do 
+  p "checking... username: #{params[:username]} password: #{params[:password]}"
+  #if exists
+  json :access_token => "tokenidveryverybig"
+end
